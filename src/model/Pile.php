5b176ba67ae65f1895af28771c4131c9f20bc4ba
Malin Lorch <ml224cv@student.lnu.se>
2018-11-14 22:12:59 -0500

    GREEN

--- src/model/Pile.php
+++ src/model/Pile.php
@@ -1,10 +1,16 @@
 <?php
 class Pile{
+    private $rowSize;
     function __construct(int $amount){
         if($amount == 4 || $amount == 5 || $amount == 6){
+            $this->rowSize = $amount;
         }else{
             throw new InvalidArgumentException();
         }
+    }
+
+    public function getRowSize(){
+        return $this->rowSize;
     }
 
 }
