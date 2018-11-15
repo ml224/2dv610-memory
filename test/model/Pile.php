3a56d3dcc05443a2b2810b2df2431a68b6263536
Malin Lorch <ml224cv@student.lnu.se>
2018-11-15 08:15:08 -0500

    REFACTOR

--- test/model/Pile.php
+++ test/model/Pile.php
@@ -5,13 +5,8 @@ require_once("../src/model/Pile.php");
 class PileTest extends TestCase
 {
 
-    public function getSut(int $amount){
+    private function getSut(int $amount){
         return new Pile($amount);
-    }
-
-    public function test_construct_instanceOfPile(){
-        $sut = $this->getSut(4);
-        $this->assertInstanceOf(Pile::class, $sut);
     }
 
     public function test_construct_invalidArgumentNegativeRange(){
