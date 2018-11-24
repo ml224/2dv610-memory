eee338106f30200eea5a9c01cdb8746c01864d59
Malin Lorch <ml224cv@student.lnu.se>
2018-11-24 09:45:11 -0500

    RED

--- test/view/GameView.php
+++ test/view/GameView.php
@@ -1,6 +1,8 @@
 <?php
 use PHPUnit\Framework\TestCase;
 require_once("../src/view/GameView.php");
+require_once("../src/model/Pile.php");
+
 
 
 class GameViewTest extends TestCase
@@ -8,6 +10,27 @@ class GameViewTest extends TestCase
     public function test_displayPile_shouldReturnString(){
         $view = new GameView();
         $this->assertInternalType($view->displayPile(), "string");
+    }
+
+    public function test_displayPile_shoulReturnStringWithArrayElements(){
+        $sut = new GameView();
+        $pile = $this->pileStub();
+        $cards = $pile->getPile();
+
+        $html = $sut->displayPile($cards);
+
+        foreach($cards as $c){
+            $this->assertRegexp('/'.$c.'/', $html);
+        }
+
+    }
+
+    private function pileStub(){
+        $stub = $this->createMock(Pile::class);
+        $stub->method('getPile')
+        ->willReturn(array("test1","test1", "test2", "test2","test4","test4", "test3", "test3"));
+
+        return $stub;
     }
 
 }
